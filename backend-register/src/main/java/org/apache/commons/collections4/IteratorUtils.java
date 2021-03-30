81ca65e7d37d6faaed1bb34f05f437231317e17b
Marlon Rodrigues <Marlonnlmx@outlook.com>
2021-03-30 17:01:42 -0300

    initial commit

---
+++ backend-register/src/main/java/org/apache/commons/collections4/IteratorUtils.java
@@ -0,0 +1,13 @@
+package org.apache.commons.collections4;
+
+import java.util.Iterator;
+import java.util.List;
+
+import app.prod.src.backend_register.entity.User;
+
+public class IteratorUtils {
+
+	public static List<User> toList(Iterator<User> iterator) {
+		return null;
+	}
+}
