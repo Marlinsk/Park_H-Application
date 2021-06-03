14f4848fe93ff3c67b1d692778eb73525a57d754
Marlon Rodrigues <Marlonnlmx@outlook.com>
2021-06-03 02:54:32 -0300

    Create and add extraclass to git

---
+++ backend-user/src/main/java/app/prod/src/backend_user/extraclass/extraclass.java
@@ -0,0 +1,43 @@
+package app.prod.src.backend_user.extraclass;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+
+public class extraclass {
+
+	/*
+	 * public encryptor(String senha) { super(); this.senha =
+	 * encryptPassword(senha); }
+	 * 
+	 * public String getSenha() { return senha; }
+	 * 
+	 * public void setSenha(String senha) { this.senha = senha; }
+	 */
+
+	protected static String encryptPassword(String password) {
+		String sha1 = "";
+		try {
+			MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+			crypt.reset();
+			crypt.update(password.getBytes("UTF-8"));
+			sha1 = byteToHex(crypt.digest());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return sha1;
+	}
+
+	protected static String byteToHex(final byte[] hash) {
+		Formatter formatter = new Formatter();
+		for (byte b : hash) {
+			formatter.format("%02x", b);
+		}
+		String result = formatter.toString();
+		formatter.close();
+		return result;
+	}
+}
