766c94ed9b0ad30412ac89c94143f3f6adc5c30e
Marlon Rodrigues <Marlonnlmx@outlook.com>
2021-06-07 14:19:37 -0300

    Create interface class UserRepository

---
+++ backend-user/src/main/java/app/prod/src/backend_user/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package app.prod.src.backend_user.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import app.prod.src.backend_user.entity.user;
+
+public interface UserRepository extends CrudRepository<user, Long> { 
+	
+     List<user> findByUsername(String username);
+     
+     List <user> findByStatename(String statename);
+     
+     List <user> findByCityname(String cityname);
+     
+}
