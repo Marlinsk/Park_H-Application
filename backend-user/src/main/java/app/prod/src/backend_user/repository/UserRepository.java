1220c0d4ec13bb40aa35bfc157db6124d795fde5
Marlon Rodrigues <Marlonnlmx@outlook.com>
2021-06-09 01:26:52 -0300

    Correction of declaration in UserRepository class

--- backend-user/src/main/java/app/prod/src/backend_user/repository/UserRepository.java
+++ backend-user/src/main/java/app/prod/src/backend_user/repository/UserRepository.java
@@ -2,18 +2,18 @@ package app.prod.src.backend_user.repository;
 
 import java.util.List;
 
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
 import app.prod.src.backend_user.entity.user;
 
-public interface UserRepository extends CrudRepository<user, Long> { 
-	
-	List<user>findById(String id);
-	 
+@EnableScan()
+public interface UserRepository extends CrudRepository<user, String> { 
+
      List<user>findByUsername(String username);
      
      List<user>findByStatename(String statename);
      
      List<user>findByCityname(String cityname);
-     
+   
 }
