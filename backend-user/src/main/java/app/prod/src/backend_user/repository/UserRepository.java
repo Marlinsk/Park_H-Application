e301373ab30898924e73b497baa225547649ce82
Marlon Rodrigues <Marlonnlmx@outlook.com>
2021-06-08 23:53:25 -0300

    Add id search filter

--- backend-user/src/main/java/app/prod/src/backend_user/repository/UserRepository.java
+++ backend-user/src/main/java/app/prod/src/backend_user/repository/UserRepository.java
@@ -8,10 +8,12 @@ import app.prod.src.backend_user.entity.user;
 
 public interface UserRepository extends CrudRepository<user, Long> { 
 	
-     List<user> findByUsername(String username);
+	List<user>findById(String id);
+	 
+     List<user>findByUsername(String username);
      
-     List <user> findByStatename(String statename);
+     List<user>findByStatename(String statename);
      
-     List <user> findByCityname(String cityname);
+     List<user>findByCityname(String cityname);
      
 }
