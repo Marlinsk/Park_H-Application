04ef6ce6d6b540b829ab0d33b8f6aaba04ca735d
Marlon <Marlonnlmx@outlook.com>
2022-07-16 14:24:20 -0300

    Changing attribute name and implementing lombok

--- backend/src/main/java/app/src/cod/backend/dtos/AnuncioDTO.java
+++ backend/src/main/java/app/src/cod/backend/dtos/AnuncioDTO.java
@@ -3,6 +3,11 @@ package app.src.cod.backend.dtos;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AnuncioDTO {
 	
 	@NotBlank
@@ -21,7 +26,7 @@ public class AnuncioDTO {
 	private String tipoVeiculo;
 	
 	@NotBlank
-	private String capacidade;
+	private String vagas;
 	
 	@NotBlank
 	private String horarioFuncionamento;
@@ -65,171 +70,4 @@ public class AnuncioDTO {
     
 	private String[] canaisPagamento;
 
-	public String getTituloAnuncio() {
-		return tituloAnuncio;
-	}
-
-	public void setTituloAnuncio(String tituloAnuncio) {
-		this.tituloAnuncio = tituloAnuncio;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public String getTipoLogradouro() {
-		return tipoLogradouro;
-	}
-
-	public void setTipoLogradouro(String tipoLogradouro) {
-		this.tipoLogradouro = tipoLogradouro;
-	}
-
-	public String getTamanhoLocal() {
-		return tamanhoLocal;
-	}
-
-	public void setTamanhoLocal(String tamanhoLocal) {
-		this.tamanhoLocal = tamanhoLocal;
-	}
-
-	public String getTipoVeiculo() {
-		return tipoVeiculo;
-	}
-
-	public void setTipoVeiculo(String tipoVeiculo) {
-		this.tipoVeiculo = tipoVeiculo;
-	}
-
-	public String getCapacidade() {
-		return capacidade;
-	}
-
-	public void setCapacidade(String capacidade) {
-		this.capacidade = capacidade;
-	}
-
-	public String getHorarioFuncionamento() {
-		return horarioFuncionamento;
-	}
-
-	public void setHorarioFuncionamento(String horarioFuncionamento) {
-		this.horarioFuncionamento = horarioFuncionamento;
-	}
-
-	public String[] getDiasdaSemana() {
-		return diasdaSemana;
-	}
-
-	public void setDiasdaSemana(String[] diasdaSemana) {
-		this.diasdaSemana = diasdaSemana;
-	}
-
-	public String getPortaria() {
-		return portaria;
-	}
-
-	public void setPortaria(String portaria) {
-		this.portaria = portaria;
-	}
-
-	public String[] getRecursosVigilancia() {
-		return recursosVigilancia;
-	}
-
-	public void setRecursosVigilancia(String[] recursosVigilancia) {
-		this.recursosVigilancia = recursosVigilancia;
-	}
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getRua() {
-		return rua;
-	}
-
-	public void setRua(String rua) {
-		this.rua = rua;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-
-	public String getComplementoLocal() {
-		return complementoLocal;
-	}
-
-	public void setComplementoLocal(String complementoLocal) {
-		this.complementoLocal = complementoLocal;
-	}
-
-	public String getTipoPagamento() {
-		return tipoPagamento;
-	}
-
-	public void setTipoPagamento(String tipoPagamento) {
-		this.tipoPagamento = tipoPagamento;
-	}
-
-	public String getPreco() {
-		return preco;
-	}
-
-	public void setPreco(String preco) {
-		this.preco = preco;
-	}
-
-	public String getTemIPTU() {
-		return temIPTU;
-	}
-
-	public void setTemIPTU(String temIPTU) {
-		this.temIPTU = temIPTU;
-	}
-
-	public String[] getCanaisPagamento() {
-		return canaisPagamento;
-	}
-
-	public void setCanaisPagamento(String[] canaisPagamento) {
-		this.canaisPagamento = canaisPagamento;
-	}
 }	
--- backend/src/main/java/app/src/cod/backend/models/Anuncio.java
+++ backend/src/main/java/app/src/cod/backend/models/Anuncio.java
@@ -1,10 +1,16 @@
 package app.src.cod.backend.models;
 
 import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
+@Setter
 @Entity
 @Table(name ="TB_ANUNCIO")
 public class Anuncio implements Serializable {
@@ -31,7 +37,7 @@ public class Anuncio implements Serializable {
 	private String tipoVeiculo;
 	
 	@Column(nullable = false, unique = false, length = 20)
-	private String capacidade;
+	private String vagas;
 	
 	@Column(nullable = false, unique = false, length = 30)
 	private String horarioFuncionamento;
@@ -48,10 +54,10 @@ public class Anuncio implements Serializable {
 	@Column(nullable = false, unique = false, length = 9)
 	private String cep;
 	
-	@Column(nullable = false, unique = false, length = 15)
+	@Column(nullable = false, unique = false, length = 30)
 	private String estado;
 	
-	@Column(nullable = false, unique = false, length = 20)
+	@Column(nullable = false, unique = false, length = 30)
 	private String cidade;
 	
 	@Column(nullable = false, unique = false, length = 70)
@@ -81,189 +87,4 @@ public class Anuncio implements Serializable {
     @Column(nullable = false)
     private LocalDateTime registrationDate;
 
-	public UUID getId() {
-		return id;
-	}
-
-	public void setId(UUID id) {
-		this.id = id;
-	}
-
-	public String getTituloAnuncio() {
-		return tituloAnuncio;
-	}
-
-	public void setTituloAnuncio(String tituloAnuncio) {
-		this.tituloAnuncio = tituloAnuncio;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
-
-	public String getTipoLogradouro() {
-		return tipoLogradouro;
-	}
-
-	public void setTipoLogradouro(String tipoLogradouro) {
-		this.tipoLogradouro = tipoLogradouro;
-	}
-
-	public String getTamanhoLocal() {
-		return tamanhoLocal;
-	}
-
-	public void setTamanhoLocal(String tamanhoLocal) {
-		this.tamanhoLocal = tamanhoLocal;
-	}
-
-	public String getTipoVeiculo() {
-		return tipoVeiculo;
-	}
-
-	public void setTipoVeiculo(String tipoVeiculo) {
-		this.tipoVeiculo = tipoVeiculo;
-	}
-
-	public String getCapacidade() {
-		return capacidade;
-	}
-
-	public void setCapacidade(String capacidade) {
-		this.capacidade = capacidade;
-	}
-
-	public String getHorarioFuncionamento() {
-		return horarioFuncionamento;
-	}
-
-	public void setHorarioFuncionamento(String horarioFuncionamento) {
-		this.horarioFuncionamento = horarioFuncionamento;
-	}
-
-	public String[] getDiasdaSemana() {
-		return diasdaSemana;
-	}
-
-	public void setDiasdaSemana(String[] diasdaSemana) {
-		this.diasdaSemana = diasdaSemana;
-	}
-
-	public String getPortaria() {
-		return portaria;
-	}
-
-	public void setPortaria(String portaria) {
-		this.portaria = portaria;
-	}
-
-	public String[] getRecursosVigilancia() {
-		return recursosVigilancia;
-	}
-
-	public void setRecursosVigilancia(String[] recursosVigilancia) {
-		this.recursosVigilancia = recursosVigilancia;
-	}
-
-	public String getCep() {
-		return cep;
-	}
-
-	public void setCep(String cep) {
-		this.cep = cep;
-	}
-
-	public String getEstado() {
-		return estado;
-	}
-
-	public void setEstado(String estado) {
-		this.estado = estado;
-	}
-
-	public String getCidade() {
-		return cidade;
-	}
-
-	public void setCidade(String cidade) {
-		this.cidade = cidade;
-	}
-
-	public String getRua() {
-		return rua;
-	}
-
-	public void setRua(String rua) {
-		this.rua = rua;
-	}
-
-	public String getBairro() {
-		return bairro;
-	}
-
-	public void setBairro(String bairro) {
-		this.bairro = bairro;
-	}
-
-	public String getNumero() {
-		return numero;
-	}
-
-	public void setNumero(String numero) {
-		this.numero = numero;
-	}
-
-	public String getComplementoLocal() {
-		return complementoLocal;
-	}
-
-	public void setComplementoLocal(String complementoLocal) {
-		this.complementoLocal = complementoLocal;
-	}
-
-	public String getTipoPagamento() {
-		return tipoPagamento;
-	}
-
-	public void setTipoPagamento(String tipoPagamento) {
-		this.tipoPagamento = tipoPagamento;
-	}
-
-	public String getPreco() {
-		return preco;
-	}
-
-	public void setPreco(String preco) {
-		this.preco = preco;
-	}
-
-	public String getTemIPTU() {
-		return temIPTU;
-	}
-
-	public void setTemIPTU(String temIPTU) {
-		this.temIPTU = temIPTU;
-	}
-
-	public String[] getCanaisPagamento() {
-		return canaisPagamento;
-	}
-
-	public void setCanaisPagamento(String[] canaisPagamento) {
-		this.canaisPagamento = canaisPagamento;
-	}
-
-	public LocalDateTime getRegistrationDate() {
-		return registrationDate;
-	}
-
-	public void setRegistrationDate(LocalDateTime registrationDate) {
-		this.registrationDate = registrationDate;
-	}
-
-	
 }
