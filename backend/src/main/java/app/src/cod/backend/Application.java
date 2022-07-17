3ababae82935e966e948216cb5e8c82b37555131
Marlon <Marlonnlmx@outlook.com>
2022-07-17 16:58:36 -0300

    Add method homePage

--- backend/src/main/java/app/src/cod/backend/Application.java
+++ backend/src/main/java/app/src/cod/backend/Application.java
@@ -2,10 +2,18 @@ package app.src.cod.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Application {
-
+	
+	@RequestMapping("/")
+	public String homePage() {
+		return "Hello World!";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
